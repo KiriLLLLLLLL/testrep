be680e053f5d6767c52190f653235917cec40a57
yanax <yanax@mail.ru>
2021-11-22 12:10:42 +0300

    add Example 003

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите два целых числа");
+string firstNum = Console.ReadLine();
+int numA = Convert.ToInt32(firstNum);
+string secondNum = Console.ReadLine();
+int numB = Convert.ToInt32(secondNum);
+int sum = numA + numB;
+Console.Write("Сумма чисел равна: " + sum);
