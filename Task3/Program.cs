4040d0aa3a72fa49f9a4bac992d0e5df81282a95
yanax <yanax@mail.ru>
2021-11-22 20:59:32 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 109;
+int b = 87;
+
+if (a > b)
+{
+    Console.WriteLine("Max = " + a);
+    Console.WriteLine("Min = " + b);
+}
+else
+{
+    Console.WriteLine("Max = " + b);
+    Console.WriteLine("Min = " + a);
+}
