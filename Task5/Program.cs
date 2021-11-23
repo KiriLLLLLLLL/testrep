7c64d6032e98eddadac3d63d9aa72ec7de6625d6
yanax <yanax@mail.ru>
2021-11-23 10:44:12 +0300

    add Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите по очереди три целых числа");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine("max = " + max);
+
