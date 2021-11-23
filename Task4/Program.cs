412ed2d0ada68b3f74f0bf4dec65cc30d6f22672
yanax <yanax@mail.ru>
2021-11-23 10:31:52 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число 1 - 7");
+string number = Console.ReadLine();
+int day = Convert.ToInt16(number);
+
+if (day == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (day == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (day == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (day == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (day == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (day == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (day == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
